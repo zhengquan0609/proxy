02d37e505baa6371b5648e1c7c2c24e89c36413d
zhengquan0609 <65141863+zhengquan0609@users.noreply.github.com>
2024-06-06 14:27:46 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,4 +1,4 @@
-#const TELEGRAPH_URL = 'https://api.openai.com';
+// const TELEGRAPH_URL = 'https://api.openai.com';
 const TELEGRAPH_URL = 'https://chromewebstore.google.com/?utm_source=ext_app_menu';
 
 
